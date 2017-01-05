eb23950167704586d9321e06a07bdc5cc452d75b
Juliane Blier <blier.juliane@gmail.com>
2017-01-05 16:50:10 +0100

    Création du Formulaire de contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,5 @@
-<span>cersei.lan@gmail.com</span>
+<form action="save.php">
+	<label for="emailFrom">Votre email : <input type="text" name="emailFrom"></label>
+	<label for="message">Message : <textarea name="message" id="" cols="30" rows="10"></textarea></label>
+	<button type="submit">Envoyer</button>
+</form>
