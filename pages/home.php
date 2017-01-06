6d019fd26ed778925b95317030661cc36a4be768
Juliane Blier <blier.juliane@gmail.com>
2017-01-06 11:18:00 +0100

    Affichage des userinfo proprement

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,13 @@
+<?php $user = getUserData('/../data/user.json'); ?>
 <h1>maison sucré maison</h1>
+
+
+
+<h2><?= $user['first_name']?> <?= $user['name']?></h2>
+<h3><?= $user['occupation']?></h3>
+<dl>	
+<?php foreach ($user['experiences'] as $value) : ?>
+	<dt>Année</dt><dd><?= $value['year']; ?></dd>
+	<dt>Entreprise</dt><dd> <?= $value['company']; ?></dd>
+<?php endforeach; ?>
+</dl>
