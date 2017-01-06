eb39732aff37fd47045c4e100b72a18f92540889
Juliane Blier <blier.juliane@gmail.com>
2017-01-06 09:33:13 +0100

    utilisation de la fonction displayLastMessage

--- public/admin.php
+++ public/admin.php
@@ -1,5 +1,5 @@
 <?php require __DIR__.'/../lib/functions.php' ?>
 <div>
 	<h2>Informations du formulaire de contact</h2>
-	<?php  var_dump(getUserData('/../data/last_message.json')) ?>
+	<?php  displayLastMessage(getUserData('/../data/last_message.json')) ?>
 </div>
